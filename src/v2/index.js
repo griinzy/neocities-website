c4cc4a9169c6ee3912f699b9d4b5b9188c9a3342
griinzy <kalin_stoqnov@abv.bg>
2025-06-15 11:42:35 +0300

    fixed the status once and for all (hopefully)
    seriously though how can so many things go wrong

--- src/v2/index.js
+++ src/v2/index.js
@@ -63,12 +63,41 @@ fetch(apiUrl)
         const user = data.data.discord_user;
         const avatarId = user.avatar;
         pfp.src = `https://cdn.discordapp.com/avatars/${userId}/${avatarId}.png`;
+        let userActivity;
+        let userStatus;
+        let foundActivityFlag = false;
         name.innerText = user.display_name;
         const activities = data.data.activities;
-        if (activities.length > 0 && activities[0].id == "custom") {
-            let statusEmoji = activities[0].emoji ? activities[0].emoji.name : "";
-            let statusText = activities[0].state ? activities[0].state : "";
-            status.innerText = (statusEmoji + " " + statusText).trim();
+
+        if(activities.length > 0) {
+            let index = 0;
+            while(index < activities.length) {
+                if(!(activities[index].name == "Spotify" || activities[index].name == "Feishin")) {
+                    if(activities[index].id == "custom") {
+                        let statusEmoji = activities[index].emoji ? activities[index].emoji.name : "";
+                        let statusText = activites[index].state ? activities[index].state : "";
+                        userStatus = (statusEmoji + " " + statusText).trim();
+                    }
+
+                    else {
+                        if(!foundActivityFlag) {
+                            userActivity = "Playing " + activities[index].name;
+                            foundActivityFlag = true;
+                        }
+                    }
+                }
+                index++;
+            }
+
+            if(userActivity && userStatus) {
+                status.innerText = userActivity + " - " + userStatus;
+            } 
+            else if(userActivity && !userStatus) {
+                status.innerText = userActivity;
+            }
+            else if(!userActivity && userStatus) {
+                status.innerText = userStatus;
+            }
         }
 
         statusIndicator.classList.remove('status-online', 'status-idle', 'status-dnd');
@@ -89,18 +118,6 @@ fetch(apiUrl)
                 break;
         }
 
-        if (activities.length > 1) {
-            let index = 0;
-            while (index < activities.length) {
-                if (activities[index].name == "Spotify" || activities[index].name == "Feishin" || activities[index].name == "Custom Status") {
-                    index++;
-                }
-                else {
-                    status.innerText = "Playing " + activities[index].name + " - " + status.innerText;
-                    break;
-                }
-            }
-        }
 
         if(data.data.listening_to_spotify) {
             let spotifyData = data.data.spotify;
