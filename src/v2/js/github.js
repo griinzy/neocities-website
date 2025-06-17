0f1072b980510a97a2fa544ce6a00d93b5cee084
griinzy <kalin_stoqnov@abv.bg>
2025-06-17 18:43:56 +0300

    added a github cache counter
    +had to update the data handling to account for the added counter

--- src/v2/js/github.js
+++ src/v2/js/github.js
@@ -1,27 +1,52 @@
 const commitPanel = document.getElementById('commit-panel');
+const lastUpdate = document.getElementById('last-update');
 
 const repo = 'neocities-website'
-const githubUrl = `https://api.gunjicordia.xyz/github/commits/${repo}`;
+// const githubUrl = `https://api.gunjicordia.xyz/github/commits/${repo}`;
+const githubUrl = `http://localhost:8002/github/commits/${repo}`;
 
 const githubTemplate = document.getElementById('github-commit-template');
 
 fetch(githubUrl)
     .then(response => response.json())
     .then(data => {
+        const commits = data.commits;
         let index = 0;
-        while(index < 5 && index < data.length) {
+        while(index < 5 && index < commits.length) {
             let separator = document.createElement('hr');
             commitPanel.appendChild(separator);
 
-            let commitUrl = data[index].html_url;
-            let commitMessage = data[index].commit.message;
-            let commitDate = new Date(data[index].commit.committer.date).toLocaleDateString('en-GB').replaceAll('/','.');
+            let commitUrl = commits[index].html_url;
+            let commitMessage = commits[index].commit.message;
+            let commitDate = new Date(commits[index].commit.committer.date).toLocaleDateString('en-GB').replaceAll('/','.');
 
             let githubCommitElem = githubTemplate.content.cloneNode(true);
             githubCommitElem.querySelector('.commit-message').href = commitUrl;
             githubCommitElem.querySelector('.commit-text').innerText = commitDate + " - " + commitMessage.split('\n')[0];
-
+            
             commitPanel.appendChild(githubCommitElem);
+
+            lastUpdate.innerText = formatTime(data.cacheAge);
+
             index++;
         }
     })
+
+function formatTime(seconds) {
+    const units = [
+        { label: "month", seconds: 2592000 },
+        { label: "day", seconds: 86400 },
+        { label: "hour", seconds: 3600 },
+        { label: "minute", seconds: 60 },
+        { label: "second", seconds: 1 },
+    ];
+
+    for (const unit of units) {
+        if (seconds >= unit.seconds) {
+            const value = Math.floor(seconds / unit.seconds);
+            return `${value} ${unit.label}${value !== 1 ? "s" : ""}`;
+        }
+    }
+
+    return "0 seconds";
+}
